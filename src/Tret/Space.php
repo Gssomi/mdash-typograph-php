9fe354bc7d48729c71e8a228e931e46bc6910f29
Gssomi <gssomi@ro.ru>
2016-11-24 20:18:22 +0600

    убрала пробел в доменных зонах

--- src/Tret/Space.php
+++ src/Tret/Space.php
@@ -48,12 +48,12 @@ class Space extends Base
 				'description'	=> 'Пробел после точки',
 				'pattern' 		=> array(
 						'/(\040|\t|\&nbsp\;|^)([a-zа-яё0-9]+)(\040|\t|\&nbsp\;)?\.([а-яёa-z]{5,})($|[^a-zа-яё])/iue',
-						'/(\040|\t|\&nbsp\;|^)([a-zа-яё0-9]+)\.([а-яёa-z]{1,4})($|[^a-zа-яё])/iue',
+						//'/(\040|\t|\&nbsp\;|^)([a-zа-яё0-9]+)\.([а-яёa-z]{1,4})($|[^a-zа-яё])/iue',
 						),
 				'replacement' 	=> array(
 						//'\1\2. \4',
 						'$m[1].$m[2]."." .( $m[5] == "." ? "" : " ").$m[4].$m[5]',
-						'$m[1].$m[2]."." .(in_array(Emuravjev\Mdash\Lib::strtolower($m[3]), $this->domain_zones)? "":( $m[4] == "." ? "" : " ")). $m[3].$m[4]'
+						//'$m[1].$m[2]."." .(in_array(Emuravjev\Mdash\Lib::strtolower($m[3]), $this->domain_zones)? "":( $m[4] == "." ? "" : " ")). $m[3].$m[4]'
 						),
 			),
 		'autospace_after_hellips' => array(
